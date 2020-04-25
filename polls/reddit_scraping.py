847792a266db15c7587a6422726de64a6cce4926
KillingJoke42 <49367372+KillingJoke42@users.noreply.github.com>
2020-04-25 17:25:21 +0530

    Add files via upload

--- polls/reddit_scraping.py
+++ polls/reddit_scraping.py
@@ -1,13 +1,19 @@
+# Get Python's Reddit API Wrapper
 import praw
+# Matplotlib will generate plots for the generated data
 from matplotlib import pyplot as plt
 
+# Generate a Reddit API Wrapper object: PRAW object to scrape reddit data
 reddit = praw.Reddit(
 						client_id="RImabBQtiUpnDw",
 						client_secret="VOhkZ1p8g215x4hY354QEKdUEn0",
 						user_agent="python-linux:text_classifier:v0.1a (by u/reddit_scraper_)"
 					)
-def get_hot_data(limit):
 
+# Get data for sort type as Hot
+def get_hot_data(limit):
+	
+	# All the flairs that were consistent with current affairs on r/India
 	link_flair_tags = {
 					"Politics":0, 
 					"Non-Political":0, 
@@ -23,14 +29,22 @@ def get_hot_data(limit):
 					"Coronavirus":0,
 				  }
 
+	# Get posts from the required sub with appropriate sorting bias
 	for submission in reddit.subreddit('india').hot(limit=limit):
+		# Check whether the received request is valid or not
 		if submission.link_flair_text:
+			# If the received flair is not part of the shortlisted flairs, Ignore it
 			try:
+				if submission.link_flair_text == "Coronavirus" and link_flair_tags[submission.link_flair_text] > 150:
+					print('Corona Exceeded')
+				else:
+					# Counting number of posts under the flair
 					link_flair_tags[submission.link_flair_text] += 1
 					flair = submission.link_flair_text
 			except KeyError:
 				print("New Flair Found: " + submission.link_flair_text + ". Ignoring..........")
 
+	# Plot the data that is scraped to display onto the webpage
 	flairs = link_flair_tags.keys()
 	values = link_flair_tags.values()
 
@@ -40,6 +54,7 @@ def get_hot_data(limit):
 	plt.savefig('polls/static/hot_graph.png')
 	return link_flair_tags
 
+# Follow same steps as hot but for sort type as new
 def get_new_data(limit):
 
 	link_flair_tags = {
